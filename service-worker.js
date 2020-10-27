4173ea08939d24ed8eece3c285fce9dc08bc9a43
yell-yoshua <usuario1529yoshua@hotmail.com>
2020-10-27 16:16:35 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0e0e9484227a4de88b2dc100ebaadd56.js"
+  "/precache-manifest.012cffa278f9566e2bb294cd2aae91d5.js"
 );
 
 self.addEventListener('message', (event) => {
